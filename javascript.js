cc1185c34b2bb0731fb02e5407577e2f43a1b093
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-03 17:14:22 +0200

    Add function that plays a single round and displays result

--- javascript.js
+++ javascript.js
@@ -1,7 +1,7 @@
 function getComputerChoice() {
-    const numberChoice1 = 'Rock'
-    const numberChoice2 = 'Paper'
-    const numberChoice3 = 'Scissors'
+    const numberChoice1 = 'rock'
+    const numberChoice2 = 'paper'
+    const numberChoice3 = 'scissors'
     const Choice = Math.floor((Math.random() * 3) + 1)
     if (Choice === 1) {
         return numberChoice1
@@ -12,5 +12,27 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice())
-
+function playSingleRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    console.log(computerSelection)
+    let playerChoice = prompt('Choose your weapon!');
+    playerSelection = playerChoice.toLowerCase();
+    if (playerSelection === computerSelection) {
+        return 'Tie!'
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats Scissors!'
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats Rock!'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beat Paper!'
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats Rock!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats Scissors!'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beat Paper!'
+    } else {
+        return 'Please draw an approved weapon! We only fight with rock, paper or scissors!'
+    }
+}
+console.log(playSingleRound())
