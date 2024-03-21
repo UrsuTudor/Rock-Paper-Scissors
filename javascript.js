dd57c5a682058607e56ff9420d0fa7bdf4904f1f
UrsuTudor <tudor_ursu90@yahoo.com>
2024-03-21 16:35:25 +0200

    Change order, remove old code and make displayEndResult into a function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,14 @@
+const gameData = document.querySelector('#results'); //main container
+
+const playBtn = document.querySelector('#play');
+
+const playerOptionBtns = document.querySelectorAll('.playerOption');
+playerOptionBtns.forEach(button => button.disabled = true);
+
+const playersChoices = document.createElement('div');
+const roundResult = document.createElement('div');
+const gameEndMsg = document.createElement('div');
+
 function getComputerChoice() {
     const numberChoice1 = 'Rock'
     const numberChoice2 = 'Paper'
@@ -12,52 +23,7 @@ function getComputerChoice() {
     }
 }
 
-const gameData = document.querySelector('#results'); //main container
-const playerOptionBtns = document.querySelectorAll('.playerOption');
-const playersChoices = document.createElement('div') ;
-const roundResult = document.createElement('div');
-const gameEndMsg = document.createElement('div')
-const playBtn = document.querySelector('#play');
-playerOptionBtns.forEach(button => button.disabled = true);
-playBtn.disabled = false;
-
-playBtn.addEventListener('click', () => {
-    playerOptionBtns.forEach(button => button.disabled = false)
-    playBtn.disabled = true
-    if (gameEndMsg.textContent == 'You win!' || gameEndMsg.textContent == 'You lose!') {
-        gameEndMsg.textContent = '';
-    }
-}) 
-
-playerOptionBtns.forEach((button) => {
-    button.addEventListener('click', () => {
-        playerSelectionGlobal = button.textContent;
-        playSingleRound();
-        keepScore();
-        if (playerScore == 5 ) {
-            playerScore = 0;
-            computerScore = 0; 
-            playerOptionBtns.forEach(button => button.disabled = true);
-            playBtn.disabled = false;
-            playBtn.textContent = 'Play Again';
-            gameEndMsg.textContent = 'You win!';
-            gameData.appendChild(gameEndMsg)
-        } else if (computerScore == 5){
-            playerScore = 0;
-            computerScore = 0; 
-            playerOptionBtns.forEach(button => button.disabled = true);
-            playBtn.disabled = false;
-            playBtn.textContent = 'Play Again';
-            gameEndMsg.textContent = 'You lose!';
-            gameData.appendChild(gameEndMsg)
-        }
-    });
-}) //makes playerOptionBtns get playerSelection and play a round
-
-let roundCount = 0
-
-let playerSelectionGlobal; //made it a global value so I could easily access it with playSingleRound(); trying to add event listeners inside the function to encase every 
-                           //bit of code inside didn't work
+let playerSelectionGlobal; //made it a global value so I could easily access it with playSingleRound();
 
 function playSingleRound(playerSelection, computerSelection) {
     playerSelection = playerSelectionGlobal;
@@ -79,7 +45,7 @@ function playSingleRound(playerSelection, computerSelection) {
         roundResult.textContent ='You win! Scissors beat Paper!'
     } 
 
-    playersChoices.textContent = 'Player: ' + playerSelection + ' ' + 'Computer: ' + computerSelection;
+    playersChoices.textContent = 'Player: ' + playerSelection + ';' + 'Computer: ' + computerSelection;
 
     gameData.appendChild(playersChoices);
     gameData.appendChild(roundResult);
@@ -103,13 +69,40 @@ keepScore = function () {
     gameData.appendChild(score)
 }
 
-function playGame() {
-    //5 keeps scores
-    if (playerScore > computerScore) {
-        console.log ('Congratulations! You beat the computer!')
-    } else if (playerScore < computerScore) {
-        console.log ('Don\'t worry! You\'ll do better next time!')
-    } else {
-        console.log ('Looks like a tie this time!')
+function displayGameResult() {
+    if (playerScore == 5 ) {
+        playerScore = 0;
+        computerScore = 0; 
+        playerOptionBtns.forEach(button => button.disabled = true);
+        playBtn.disabled = false;
+        playBtn.textContent = 'Play Again';
+        gameEndMsg.textContent = 'You win!';
+        gameData.appendChild(gameEndMsg)
+    } else if (computerScore == 5){
+        playerScore = 0;
+        computerScore = 0; 
+        playerOptionBtns.forEach(button => button.disabled = true);
+        playBtn.disabled = false;
+        playBtn.textContent = 'Play Again';
+        gameEndMsg.textContent = 'You lose!';
+        gameData.appendChild(gameEndMsg)
     }
 }
+playBtn.addEventListener('click', () => {
+    playerOptionBtns.forEach(button => button.disabled = false);
+
+    playBtn.disabled = true;
+
+    if (gameEndMsg.textContent == 'You win!' || gameEndMsg.textContent == 'You lose!') {
+        gameEndMsg.textContent = '';
+    }
+}) 
+
+playerOptionBtns.forEach((button) => {
+    button.addEventListener('click', () => {
+        playerSelectionGlobal = button.textContent;
+        playSingleRound();
+        keepScore();
+        displayGameResult();
+    });
+}) //makes playerOptionBtns get playerSelection, play a round, display the Score and give an end game message once the score reaches 5 
